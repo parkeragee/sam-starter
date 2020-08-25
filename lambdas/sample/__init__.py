a03566111b155ad3101c63c758afb3d0d3c87b45
Parker Agee <parker@parkeragee.com>
2020-08-25 17:03:19 -0500

    Add files via upload

---
+++ lambdas/sample/__init__.py
@@ -0,0 +1,10 @@
+def handle(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET"
+        },
+        "body": json.dumps({"message": "Sample"})
+    }
